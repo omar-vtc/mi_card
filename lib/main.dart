8274f19989017251c133bf1892f6c9ca8075692a
omar-vtc <72626558+omar-vtc@users.noreply.github.com>
2024-11-06 18:41:35 +0200

    Finalized

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.grey[900],
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(
                 radius: 50.0,
@@ -36,59 +37,52 @@ class MyApp extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
               ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal[100],
+                ),
+              ),
               Card(
-                color: Colors.white,
                 margin: EdgeInsets.symmetric(
                   vertical: 10.0,
                   horizontal: 25.0,
                 ),
                 // padding: EdgeInsets.all(10.0),
-                child: Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.phone,
-                        color: Colors.teal,
-                      ),
-                      SizedBox(width: 10.0),
-                      Text(
-                        '010 90 28 3565',
-                        style: TextStyle(
-                          color: Colors.teal[900],
-                          fontFamily: 'SourceSans',
-                          fontSize: 20.0,
-                        ),
-                      )
-                    ],
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+20 10 9028 3565',
+                    style: TextStyle(
+                      color: Colors.teal[900],
+                      fontFamily: 'SourceSans',
+                      fontSize: 20.0,
+                    ),
                   ),
                 ),
               ),
               Card(
-                color: Colors.white,
                 margin: EdgeInsets.symmetric(
                   vertical: 10.0,
                   horizontal: 25.0,
                 ),
                 // padding: EdgeInsets.all(10.0),
-                child: Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.email,
-                        color: Colors.teal,
-                      ),
-                      SizedBox(width: 10.0),
-                      Text(
-                        'komar4727@gmail.com',
-                        style: TextStyle(
-                          color: Colors.teal[900],
-                          fontFamily: 'SourceSans',
-                          fontSize: 20.0,
-                        ),
-                      )
-                    ],
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'komar4727@gmail.com',
+                    style: TextStyle(
+                      color: Colors.teal[900],
+                      fontFamily: 'SourceSans',
+                      fontSize: 20.0,
+                    ),
                   ),
                 ),
               )
